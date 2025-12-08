4bcae6b5372aaabd41ee8a61b39197007c8eac3d
bossm0n5t3r <bossm0n5t3r@gmail.com>
2025-12-08 23:45:00 +0900

    feat: configure toolchains and add JSON serialization support
    - Set JVM toolchain to Java 21 with support for JVM 17 target
    - Add `kotlinx-serialization-json` dependency for JSON serialization
    - Configure Kotlin and Java compile tasks to use appropriate JVM versions
    - Enable `maven-publish` and signing plugins

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,38 @@
+import org.gradle.jvm.toolchain.JavaLanguageVersion
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
     alias(libs.plugins.kotlin.jvm)
     alias(libs.plugins.kotlin.serialization)
     alias(libs.plugins.ktlint.gradle)
+
+    `maven-publish`
+    signing
 }
 
 group = "io.github.bossm0n5t3r"
 version = "1.0-SNAPSHOT"
+
+kotlin {
+    jvmToolchain(21)
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(21))
+    }
+}
+
+tasks.withType<KotlinCompile>().configureEach {
+    compilerOptions {
+        jvmTarget.set(JvmTarget.JVM_17)
+    }
+}
+
+tasks.withType<JavaCompile>().configureEach {
+    options.release.set(17)
+}
 
 repositories {
     mavenCentral()
@@ -14,11 +41,9 @@ repositories {
 dependencies {
     implementation(libs.kotlin.stdlib)
     implementation(libs.kotlinx.serialization.core)
-    testImplementation(libs.kotlin.test.junit5)
-}
 
-kotlin {
-    jvmToolchain(21)
+    testImplementation(libs.kotlin.test.junit5)
+    testImplementation(libs.kotlinx.serialization.json)
 }
 
 tasks.test {
